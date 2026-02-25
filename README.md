62ee43418bcf344292d49ae924838e27daa971f9
masafumi-sasahara <masafumi@sasahara.io>
2026-02-25 23:25:19 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# create-where-bridge
+Scaffold a new where-bridge project.
+
+## Usage
+```sh
+npm create where-bridge@latest [where-brige-name]
+```
+
---
+++ index.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+import path from 'node:path';
+import url from 'node:url';
+
+import { parseArgs } from 'node:util';
+
+import fs from 'fs-extra';
+import prompts from 'prompts';
+
+const main = async () => {
+
+  // package
+  const bridgePackage = { name: '@where-org/where-bridge', version: '^1.0.0', };
+
+  // args
+  const { positionals } = parseArgs({
+
+    allowNegative: true,
+    allowPositionals: true,
+
+  });
+
+  const [name] = positionals;
+
+  // prompt
+  const result = await prompts([{
+    // name
+    type   : name ? null : 'text',
+    name   : 'name',
+    message: ':Please enter the where-bridge name:',
+    initial: 'where-bridge'
+  }]);
+
+  const packageName = name ?? result.name;
+
+  const srcDir = path.resolve(import.meta.dirname, 'templates'),
+        destDir = path.resolve(process.cwd(), packageName);
+
+  fs.copySync(srcDir, destDir);
+
+  // package.json
+  const packageJsonPath = path.resolve(destDir, 'package.json'),
+        packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
+
+  packageJson.dependencies[bridgePackage.name] = bridgePackage.version;
+
+  fs.writeFileSync(
+    packageJsonPath, JSON.stringify({ ...packageJson, name: packageName }, null, 2)
+  );
+
+  console.log(`
+  cd ${packageName}
+  npm i
+  npm start`);
+
+};
+
+main();
